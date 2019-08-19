bbda51bdfc8de9133a6c769565cc08b7e78eeeb6
Teemu Myllärinen <teemuerik@gmail.com>
2019-08-19 14:10:54 +0300

    commit

--- CodeBot.py
+++ CodeBot.py
@@ -4,13 +4,14 @@ import asyncio
 import discord
 import json
 import os
+import re
 
 from discord.ext import commands
 from discord.utils import get
 
 TOKEN = "NjEwNzkwMDk0NjA5MDU1Nzc3.XVKZ2g.QPtPHeZ4mAm1vT9Y3EVqzWQcsGw"
 
-owner = "Mäntänmakkara#5107"
+MAX_MESSAGE_LEN = 2000
 
 bot = commands.Bot(command_prefix="$")
 
@@ -41,6 +42,8 @@ async def get_config(guild_id) -> ServerConfig:
 async def set_config(config, guild_id):
     with open(str(guild_id) + ".json", "w") as f:
         json.dump(await config.to_json(), f)
+
+# Role selector
 
 @bot.command(name="role_message", pass_context=True)
 @commands.has_permissions(administrator=True)
@@ -105,5 +108,136 @@ async def on_member_join(member: discord.Member):
     role: discord.Role = get(member.guild.roles, name="Code Monkey")
     print(f"Adding default role to member \"{member.display_name}#{member.discriminator}\"")
     await member.add_roles(role)
+
+# Code file sender
+
+def get_file_settings():
+    with open("file_settings.json", "r") as f:
+        return json.load(f)
+
+def get_ext_settings(ext):
+    settings = get_file_settings()
+    if ext not in settings.keys():
+        return settings["other"]
+    return settings[ext]
+
+def split_string_at(string, *indices):
+    if len(indices) == 0:
+        return [string]
+    index_list = list(indices)
+    if index_list[0] != 0:
+        index_list.insert(0, 0)
+    index_list.append(None)
+    parts = [string[index_list[i]:index_list[i+1]] for i in range(len(index_list)-1)]
+    return parts
+
+def get_split_messages(file_content: str, ext):
+    file_content.replace("    ", "\t")
+    if len(file_content.rstrip()) <= MAX_MESSAGE_LEN:
+        return file_content
+    settings = get_ext_settings(ext)
+    split_re = settings["message_split_regex"]
+    split_i = settings["split_index"]
+    
+    positions = []
+    max_priority = 0
+    if "priority" not in split_re:
+        # Get RegEx match positions.
+        regex = re.compile(split_re)
+        matches = regex.findall(file_content)
+        for match in matches:
+            if split_i < 0:
+                positions.append((0, match.endpos + split_i + 1))
+            else:
+                positions.append((0, match.pos + split_i))
+    else:
+        curr_priority = 0
+        while True:
+            # Get RegEx match positions.
+            regex = re.compile(split_re.replace("priority", str(curr_priority)))
+            matches = regex.findall(file_content)
+            if len(matches) == 0:
+                max_priority = curr_priority - 1
+                break
+            for match in matches:
+                if split_i < 0:
+                    positions.append((curr_priority, match.endpos + split_i + 1))
+                else:
+                    positions.append((curr_priority, match.pos + split_i))
+
+    # Set split positions to the start of the line.
+    new_positions = []
+    for pos in positions:
+        while pos[1] != 0:
+            pos[1] -= 1
+            if file_content[pos[1]] == '\n':
+                pos[1] += 1
+                new_positions.append(pos)
+                break
+    all_parts = split_string_at(file_content, *[np[1] for np in new_positions])
+    if len(all_parts) > 1:
+        split_parts = [(0, all_parts[0])].extend(zip([np[0] for np in new_positions], all_parts[1:]))
+    else:
+        split_parts = [(0, all_parts[0])]
+
+    # Split parts longer than the max length.
+    parts = []
+    for p in split_parts:
+        while len(p[1].rstrip()) > MAX_MESSAGE_LEN:
+            for x in range(MAX_MESSAGE_LEN):
+                i = MAX_MESSAGE_LEN - x
+                if p[1][i] == '\n':
+                    p_priority = p[0]
+                    p_split, p = split_string_at(p[1], i)
+                    parts.append((p_priority, p_split))
+        parts.append(p)
+    
+    # Join parts shorter than the max length.
+    grouped_parts = []
+    new_part = (0, "")
+    for rpriority in range(max_priority + 1):
+        priority = max_priority - rpriority
+        for p in parts:
+            if new_part == (0, ""):
+                new_part = (priority - 1, p[1])
+            if p[0] == priority:
+                if len(new_part[1]) + len(p[1]) < MAX_MESSAGE_LEN:
+                    new_part = (priority - 1, ''.join([new_part[1], p[1]]))
+                elif len(new_part[1]) + len(p[1].rstrip()):
+                    new_part = (priority - 1, ''.join([new_part[1], p[1].rstrip()]))
+                else:
+                    grouped_parts.append(new_part)
+                    new_part = (0, "")
+            else:
+                grouped_parts.append(new_part)
+                new_part = (0, "")
+    
+    # Return final messages.
+    return [p[1].rstrip() for p in grouped_parts]
+
+@bot.command(name="code", pass_context=True)
+async def send_code_file(ctx):
+    if len(ctx.message.attachments) == 0:
+        msg = await ctx.send("Liitä viestiin tiedosto jonka haluat lähettää.\n(Viesti poistetaan automaattisesti.)")
+        await asyncio.sleep(10)
+        await ctx.message.delete()
+        await msg.delete()
+        return
+    attachment: discord.Attachment = ctx.message.attachments[0]
+    file_bytes: bytes = await attachment.read()
+    try:
+        content = file_bytes.decode()
+    except UnicodeDecodeError:
+        msg = await ctx.send("Liitetiedostoa ei voitu lukea.\n(Viesti poistetaan automaattisesti.)")
+        await asyncio.sleep(10)
+        await ctx.message.delete()
+        await msg.delete()
+        return
+    filename = attachment.filename
+    ext = filename.split(".")[1]
+    messages = get_split_messages(content, ext)
+    await ctx.send(f"{ctx.user.mention} lähetti tiedoston \"{filename}\".")
+    for message in messages:
+        await ctx.send(f"```{ext}\n{message}```")
 
 bot.run(TOKEN)
